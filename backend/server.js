c9c5e06f6f21f1a6370699850eaab8db35a3bd21
Aman Patwa <amanpatwa234@gmail.com>
2026-05-22 00:27:40 +0530

    Initial SHYNIA CI/CD setup

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,13 @@ const { sequelize } = require('./models');
 
 const app = express();
 
-app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
+app.use(cors({
+  origin: [
+    'http://YOUR_PUBLIC_IP',
+    'http://YOUR_PUBLIC_IP:5173'
+  ],
+  credentials: true
+}));
 app.use(express.json());
 app.use('/uploads', express.static('uploads'));
 
